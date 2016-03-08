8d26352d9553c17358ff0e92ebbc912bbc9dcb78
apple <zhao@appledeMacBook-Pro.local>
2016-03-08 09:24:02 +0800

    123

---
+++ QuickStart/QuickStart/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  QuickStart
+//
+
+//  Copyright (c) 2016年 xincheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
